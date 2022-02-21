bf2720154ba6831589ddfab32cc951d3f91b77e6
Your Name <hongnhunghalk013@gmail.com>
2022-02-21 11:18:41 -0800

    bcypt final

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-require('dotenv').config()
 //console.log(process.env)
+//configure dovenv dont forget to put .env in gitignore
+
+require('dotenv').config()
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt=require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -16,61 +19,67 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 ///////////////////////////////////////////////////
-const uri="mongodb://localhost:27017/userDB"
+const uri = "mongodb://localhost:27017/userDB"
 // const uri =
 //   "mongodb+srv://hongn:mznLLqQF5og4ZvU1@cluster0.nhfgr.mongodb.net/blogDB?retryWrites=true&w=majority";
 mongoose.connect(uri);
-const userSchema=new mongoose.Schema({
-  email:String,
-  password:String
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
 });
-////////////////////////database encryption////////////////////////////
+////////////////////////encryption////////////////////////////
 //console.log(process.env.API_KEY);//access to environment variables
-const secret=process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields:['password'] });
+const secret = process.env.SECRET;
 ////////////////////////////////////////////////////
-const User=new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 ////////////////////////////////////////////////////////////////////////
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.render("home");
 });
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
   res.render("login");
 });
 
-app.get('/register',(req,res)=>{
+app.get('/register', (req, res) => {
   res.render("register");
 });
 
-app.post('/register',(req,res)=>{
-  const newUser=new User({
-    email:  req.body.username,
-    password: req.body.password
+app.post('/register', (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save((err) => {
+      if (err) console.log(err);
+      else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save((err)=>{
-    if(err) console.log(err);
-    else{
-      res.render("secrets");
-    }
-  })
+
 });
 
-app.post('/login',(req,res)=>{
-  const username=req.body.username;
-  const password=req.body.password;
-  User.findOne({email:username},(err,result)=>{
-    if(err) console.log(err);
-    else if(result) {
-      if(result.password===password){
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({
+    email: username
+  }, (err, result) => {
+    if (err) console.log(err);
+    else if (result) {
+      bcrypt.compare(password, result.password, function(err, result) {
+        if (result) {
           res.render("secrets");
-          console.log(password);//this security is bad
-      }else console.log("not password");
-    }else console.log("no user found");
+          console.log(result.password);
+        } else console.log("wrong password");
+      });
+    } else console.log("no user found");
   });
 });
-
 
 
 
